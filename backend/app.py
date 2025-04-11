a15dbb3de465572eac69010c99b8592eaf51524c
Tamara <44346875+Tamaaraa@users.noreply.github.com>
2025-04-11 15:38:04 +0200

    Linting fixes

--- backend/app.py
+++ backend/app.py
@@ -1,23 +1,27 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-from supabase import create_client, Client
 import os
-from dotenv import load_dotenv
-from flask_socketio import SocketIO, emit, join_room, leave_room
-from datetime import datetime 
+from datetime import datetime
 import requests
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_socketio import SocketIO, emit, join_room, leave_room
+from supabase import Client, create_client
 
 load_dotenv()
 
+
 def create_app(app=None, env=None):
     app = Flask(__name__)
-    CORS(app, resources={r"/api/*": {"origins": [os.environ.get("FRONT_END_URL")]}})
-    socketio = SocketIO(app, cors_allowed_origins=[os.environ.get("FRONT_END_URL")])
+    CORS(app, resources={r"/api/*": {
+        "origins": [os.environ.get("FRONT_END_URL")]
+        }})
+    socketio = SocketIO(app,
+                        cors_allowed_origins=[os.environ.get("FRONT_END_URL")])
 
     url: str = os.environ.get("SUPA_URL")
     key: str = os.environ.get("SUPA_KEY")
     supabase: Client = create_client(url, key)
-    
+
     initialize_users(supabase)
 
     @app.route("/api/register", methods=["POST"])
@@ -31,19 +35,30 @@ def create_app(app=None, env=None):
             return jsonify({"error": "Missing fields"}), 400
 
         try:
-            response = supabase.auth.sign_up({"email": email, "password": password, "options": { "data": {"display_name": username}}})
+            response = supabase.auth.sign_up({"email": email,
+                                              "password": password,
+                                              "options": {"data": {
+                                                  "display_name": username
+                                                  }}})
             if not response or not response.session:
                 return jsonify({"error": "Invalid email or password"}), 400
-            
-            supabase.table("users").insert([{ "user_id": response.user.id, "display_name": username }]).execute()
-            return jsonify({"message": "Sign up successful", "token": response.session.access_token, "username": response.user.user_metadata["display_name"], "user_id": response.user.id}), 201
-        
+
+            supabase.table("users").insert([{"user_id": response.user.id,
+                                             "display_name": username}]
+                                           ).execute()
+            return jsonify({
+                "message": "Sign up successful",
+                "token": response.session.access_token,
+                "username": response.user.user_metadata["display_name"],
+                "user_id": response.user.id
+                }), 201
+
         except Exception as e:
             print(f"Login error: {e}")
             return jsonify({"error": f"Sign up error: {(str(e))}"}), 400
 
     @app.route("/api/login", methods=["POST"])
-    def login(): 
+    def login():
         data = request.json
         email = data.get("email")
         password = data.get("password")
@@ -52,7 +67,10 @@ def create_app(app=None, env=None):
             return jsonify({"error": "Missing fields"}), 400
 
         try:
-            response = supabase.auth.sign_in_with_password({"email": email, "password": password})
+            response = supabase.auth.sign_in_with_password({
+                "email": email,
+                "password": password
+                })
 
             if not response or not response.session:
                 return jsonify({"error": "Invalid email or password"}), 400
@@ -71,7 +89,8 @@ def create_app(app=None, env=None):
         error = verify_token()
         if error:
             return error
-        rooms = supabase.table("room").select("*").order("created_at", desc=False).execute()
+        rooms = supabase.table("room").select("*").order("created_at",
+                                                         desc=False).execute()
         return jsonify(rooms.data)
 
     @app.route("/api/rooms/create", methods=["POST"])
@@ -79,7 +98,7 @@ def create_app(app=None, env=None):
         user = verify_token(return_user=True)
         if not user:
             return jsonify({"error": "Invalid token"}), 401
-        
+
         data = request.json
         creator_id = user.id
         room_name = data.get("name")
@@ -88,7 +107,11 @@ def create_app(app=None, env=None):
         if not room_name:
             return jsonify({"error": "Room name is required"}), 400
 
-        res = supabase.table("room").insert({"name": room_name, "description": room_desc, "room_creator": creator_id}).execute()
+        res = supabase.table("room").insert({
+            "name": room_name,
+            "description": room_desc,
+            "room_creator": creator_id
+            }).execute()
         return jsonify(res.data), 201
 
     @app.route("/api/rooms/<room_id>", methods=["GET"])
@@ -96,13 +119,13 @@ def create_app(app=None, env=None):
         user = verify_token(return_user=True)
         if not user:
             return jsonify({"error": "Invalid token"}), 401
-        
+
         res = supabase.table("room").select("*").eq("id", room_id).execute()
         if not res:
             return jsonify({"error": "Room doesn't exist"}), 404
-        
+
         res.data[0]["user_id"] = user.id
-        
+
         return jsonify(res.data[0]), 200
 
     @app.route("/api/rooms/<room_id>/messages", methods=["GET"])
@@ -115,7 +138,9 @@ def create_app(app=None, env=None):
             .execute()
         )
 
-        sender_ids = {msg["sender_id"] for msg in messages.data}
+        sender_ids = {
+            msg["sender_id"] for msg in messages.data
+        }
         users = (
             supabase.table("users")
             .select("user_id, display_name")
@@ -123,10 +148,13 @@ def create_app(app=None, env=None):
             .execute()
         )
 
-        user_dic = {user["user_id"]: user["display_name"] for user in users.data}
+        user_dic = {
+            user["user_id"]: user["display_name"] for user in users.data
+        }
 
         for message in messages.data:
-            message["display_name"] = user_dic.get(message["sender_id"], "Unknown User")
+            message["display_name"] = user_dic.get(message["sender_id"],
+                                                   "Unknown User")
 
         return jsonify(messages.data)
 
@@ -166,10 +194,15 @@ def create_app(app=None, env=None):
         video_url += "?autoplay=1&showinfo=0&controls=0"
 
         try:
-            response = requests.get(f"https://www.youtube.com/oembed?url=https://www.youtube.com/watch?v={video_id}&format=json")
+            video_json = f"https://www.youtube.com/watch?{video_id}"
+            response = requests.get(
+                f"https://www.youtube.com/oembed?url={video_json}&format=json")
             response.raise_for_status()
             video_title = response.json().get("title", "Unknown Title")
-            video_thumbnail= response.json().get("thumbnail_url", "https://cdn-icons-png.freepik.com/512/683/683935.png")
+            video_thumbnail = response.json().get(
+                "thumbnail_url",
+                "https://cdn-icons-png.freepik.com/512/683/683935.png"
+            )
         except Exception as e:
             print(f"Error fetching video title: {e}")
             video_title = "Unknown Title"
@@ -182,7 +215,9 @@ def create_app(app=None, env=None):
             .limit(1)
             .execute()
         )
-        next_position = (max_position.data[0]["position"] + 1) if max_position.data else 1
+        next_position = (
+            max_position.data[0]["position"] + 1
+            ) if max_position.data else 1
 
         supabase.table("video_queue").insert({
             "room_id": room_id,
@@ -208,7 +243,7 @@ def create_app(app=None, env=None):
 
         supabase.table("video_queue").delete().eq("id", video_id).execute()
         return jsonify({"message": "Video removed from queue"}), 200
-    
+
     @app.route("/api/rooms/<room_id>/delete", methods=["DELETE"])
     def delete_room(room_id):
         error = verify_token()
@@ -225,11 +260,11 @@ def create_app(app=None, env=None):
             return error
 
         return jsonify({"message": "Token is valid"}), 200
-    
+
     def verify_token(token=None, return_user=False):
         if not token:
             token = request.headers.get("Authorization")
-            
+
             if not token:
                 return jsonify({"error": "Unauthorized"}), 401
 
@@ -256,7 +291,7 @@ def create_app(app=None, env=None):
         join_room(room_id)
 
     @socketio.on("message")
-    def handle_message(data): 
+    def handle_message(data):
         token = data["token"]
         user = verify_token(token, return_user=True)
 
@@ -303,10 +338,14 @@ def create_app(app=None, env=None):
 
         if next_video.data:
             video = next_video.data[0]
-            supabase.table("room").update({"room_thumbnail": video["video_thumbnail"]}).eq("id", room_id).execute()
+            supabase.table("room").update({
+                "room_thumbnail": video["video_thumbnail"]
+                }).eq("id", room_id).execute()
             emit("play_video", {"video_url": video["video_url"]}, room=room_id)
 
-            supabase.table("video_queue").delete().eq("id", video["id"]).execute()
+            supabase.table("video_queue").delete().eq(
+                "id", video["id"]
+            ).execute()
 
     return app, socketio
 
@@ -314,13 +353,20 @@ def create_app(app=None, env=None):
 def initialize_users(supabase):
     """Creates 2 testing accounts if they don't exist."""
     users = [
-        {"email": "testing1@gmail.com", "password": os.environ.get("TEST_ACC_1_PASS"), "username": "Testing1"},
-        {"email": "testing2@gmail.com", "password": os.environ.get("TEST_ACC_2_PASS"), "username": "Testing2"}
+        {"email": "testing1@gmail.com",
+         "password": os.environ.get("TEST_ACC_1_PASS"),
+         "username": "Testing1"},
+        {"email": "testing2@gmail.com",
+         "password": os.environ.get("TEST_ACC_2_PASS"),
+         "username": "Testing2"}
     ]
 
     for user in users:
         try:
-            existing_user = supabase.table("users").select("*").eq("display_name", user["username"]).execute()
+            existing_user = supabase.table("users").select("*").eq(
+                "display_name",
+                user["username"]
+                ).execute()
             if not existing_user.data:
                 response = supabase.auth.sign_up({
                     "email": user["email"],
